da11daf0f0361533535274f79c94facb64380c24
Piotr Pepek <piotr@parasoft.com>
2022-04-11 12:40:08 +0200

    Initial version

---
+++ goo_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "goo.h"
+
+// 
+TEST(GooTest, TestGetValueWithZero) {
+  EXPECT_EQ(getValue(0, 0), 0);
+}
+
+// 
+TEST(GooTest, TestGetValueWithPositive) {
+  EXPECT_EQ(getValue(5, 5), 25);
+}
