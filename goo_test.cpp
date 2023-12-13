85c6eaad96f7052cf1348b1295345e84d3dcb1a6
Piotr Pepek <71152960+piotrpepek@users.noreply.github.com>
2023-12-13 02:56:07 +0000

    Remove test case

--- goo_test.cpp
+++ goo_test.cpp
@@ -16,11 +16,3 @@ TEST(GooTest, TestGetValueWithPositive) {
 
   EXPECT_EQ(getValue(5, 5), 25);
 }
-
-// 
-TEST(GooTest, TestGetValueWithMix) {
-  RecordProperty("cpptest_filename", __FILE__);
-  RecordProperty("req", "CIC-6");
-
-  EXPECT_EQ(getValue(5, -5), 5);
-}
